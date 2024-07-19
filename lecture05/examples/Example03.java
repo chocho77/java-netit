867d16c208d81c6338075169dbafdb615fef7c94
chocho77 <chavmom@gmail.com>
2024-07-19 13:20:41 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   lecture05/examples/Example02.java 	new file:   lecture05/examples/Example03.java

---
+++ lecture05/examples/Example03.java
@@ -0,0 +1,23 @@
+package examples;
+
+public class Example03 {
+    public static int getDecimal(int binary) {
+        int decimal = 0;
+        int n = 0;
+        while(true){
+            if (binary == 0) {
+                break;
+            } else {
+                int temp = binary % 10;
+                decimal += temp*Math.pow(2, n);
+                binary = binary / 10;
+                n++;
+            }
+        }
+
+        return decimal;
+    }
+    public static void main(String[] args) {
+        System.out.println(getDecimal(11011));
+    }
+}
