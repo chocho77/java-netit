47095d81073860dda88e28f0f653e33ddc636214
chocho77 <chavmom@gmail.com>
2024-07-19 15:37:35 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   lecture10/examples/Example01.java 	new file:   lecture10/examples/Example02.java 	new file:   lecture10/examples/Example03.java

---
+++ lecture10/examples/Example01.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class Example01 {
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 7, 4, 5, 2};
+        for(int i = numbers.length - 1; i>= 0; i--){
+            System.out.print(numbers[i] + " ");
+        } 
+    }
+}
---
+++ lecture10/examples/Example02.java
@@ -0,0 +1,46 @@
+package examples;
+
+import java.util.Arrays;
+
+public class Example02 {
+    public static void main(String[] args) {
+        int[] arr = { 5, -2, 23, 7, 87, -42, 509 };
+        System.out.println("The original array is: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        Arrays.sort(arr);
+        System.out.println("\nThe sorted array is: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        System.out.print("End Program.\n");
+
+        
+    }
+    
+}
+
+
+/*
+ * 
+ * import java.util.Arrays;
+
+class GFG {
+    public static void main(String args[])
+    {
+        int[] arr = { 5, -2, 23, 7, 87, -42, 509 };
+        System.out.println("The original array is: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        Arrays.sort(arr);
+        System.out.println("\nThe sorted array is: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
+ */
