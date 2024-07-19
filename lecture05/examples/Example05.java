0cdf9385468b20d0949d43861f75ba017f53e3c3
chocho77 <chavmom@gmail.com>
2024-07-19 15:08:17 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   lecture05/examples/Example05.java

--- lecture05/examples/Example05.java
+++ lecture05/examples/Example05.java
@@ -10,11 +10,19 @@ public class Example05 {
 
     }
 
+    private static int count(int num){
+        System.out.print(num + " ");
+        return (num > 0 ? count(num - 1): 0);
+    }
+
     public static void main(String[] args) {
         
         Example05.parseInteger(1,2,3);
         Example05.parseInteger(34,4,3,4,4);
         Example05.parseInteger(23, 45,33,22,12,33,12,34,556,54);
+        Example05.count(6);
+        System.out.print("End Program.");
+        System.out.println();
 
     }
     
